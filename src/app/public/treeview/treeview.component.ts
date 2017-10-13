c3c019814ce642b36ec29ef766b7f396e6a721aa
Vexth <oo932758@sina.com>
2017-10-13 10:26:42 +0800

    修改

--- src/app/public/treeview/treeview.component.ts
+++ src/app/public/treeview/treeview.component.ts
@@ -10,9 +10,6 @@ import { EventNameService } from '../../business/services/communication.service'
   selector: 'app-treeview',
   templateUrl: './treeview.component.html',
   styleUrls: [
-    // '../login/login.component.css',
-    // '../login/login.css',
-    // '../../../assets/plugins/iCheck/square/blue.css',
     './treeview.component.css'
   ]
 })
@@ -107,108 +104,4 @@ export class TreeviewComponent implements OnInit {
     })
   }
 
-  /*
-  onDingSignin(token: string) {
-    this.GetList.dingSignin(token).then(res => {
-      if (res.code == 0) {
-        this.EventNameService.eventName.next(res.data.name);//存放姓名
-        sessionStorage.setItem("name", res.data.name);
-        
-        this.router.navigate(['/']).then(() => {
-          this.service.GetAuthlist().then(res => {
-            this.styleName1 = '';
-            this.styleName2 = 'vexth';
-            for(let i = 0; i < res.authList.length; i++){
-              switch (res.authList[i].id) {
-              case 1:
-                res.authList[i].icon = this.ICON[0];
-                break;
-              case 13:
-                res.authList[i].icon = this.ICON[1];
-                break;
-              case 14:
-                res.authList[i].icon = this.ICON[2];
-                break;
-              case 15:
-                res.authList[i].icon = this.ICON[3];
-                break;
-              
-              }
-              
-            }
-            this.names = res.authList;
-            this.check = this.treemodule + this.treeitem;
-            sessionStorage.setItem("key", this.styleName2);
-            sessionStorage.setItem("vexth", JSON.stringify(this.names));
-
-            this.msgs = [];
-            this.msgs = [{ severity: 'info', summary: '成功', detail: '成功' }];
-
-          })
-        })
-      } else {
-        this.msgs = [];
-        this.msgs.push({ severity: 'error', summary: '错误提示', detail: res.msg });
-      }
-    })
-  }
-  */
-
-  /*
-  onSubmit(formValue: any): void {
-    if (formValue.username == undefined) {
-      this.msgs = [];
-      this.msgs.push({ severity: 'error', summary: '错误提示', detail: '请填写用户名' });
-      return ;
-    }
-    if (formValue.password == undefined) {
-      this.msgs = [];
-      this.msgs.push({ severity: 'error', summary: '错误提示', detail: '请填写密码' });
-      return ;
-    }
-    this.router.navigate(['/']);
-    sessionStorage.removeItem('/');
-    this.service.signin(formValue.username, formValue.password).then(auth => {
-      if (auth.status == 200) {
-        auth = auth.json();
-        this.EventNameService.eventName.next(auth.data.name);//存放姓名
-        sessionStorage.setItem("name", auth.data.name);
-        this.msgs = [];
-        this.msgs = [{ severity: 'info', summary: '成功', detail: '成功' }];
-        this.router.navigate(['/']).then(() => {
-          this.service.GetAuthlist().then(res => {
-            this.styleName1 = '';
-            this.styleName2 = 'vexth';
-            for(let i = 0; i < res.authList.length; i++){
-              switch (res.authList[i].id) {
-              case 1:
-                res.authList[i].icon = this.ICON[0];
-                break;
-              case 13:
-                res.authList[i].icon = this.ICON[1];
-                break;
-              case 14:
-                res.authList[i].icon = this.ICON[2];
-                break;
-              case 15:
-                res.authList[i].icon = this.ICON[3];
-                break;
-              
-              }
-
-            }
-            this.names = res.authList;
-            this.check = this.treemodule + this.treeitem;
-            sessionStorage.setItem("key", this.styleName2);
-            sessionStorage.setItem("vexth", JSON.stringify(this.names));
-          })
-        })
-      } else {
-        this.msgs = [];
-        auth = auth.json();
-        this.msgs.push({ severity: 'error', summary: '错误提示', detail: auth.msg });
-      }
-    });
-  }
-  */
 }
